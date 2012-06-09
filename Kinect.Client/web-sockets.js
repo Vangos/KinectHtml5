8a8d4106906208b943732f332adfc153fcedfccd
Vangos Pterneas <pterneas@gmail.com>
2012-06-09 21:31:40 +0300

    Initial release

---
+++ Kinect.Client/web-sockets.js
@@ -0,0 +1,53 @@
+﻿window.onload = function () {
+    var status = document.getElementById("status");
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+
+    if (!window.WebSocket) {
+        status.innerHTML = "Your browser does not support web sockets!";
+        return;
+    }
+
+    status.innerHTML = "Connecting to server...";
+
+    // Initialize a new web socket.
+    var socket = new WebSocket("ws://localhost:8181/KinectHtml5");
+
+    // Connection established.
+    socket.onopen = function () {
+        status.innerHTML = "Connection successful.";
+    };
+
+    // Connection closed.
+    socket.onclose = function () {
+        status.innerHTML = "Connection closed.";
+    }
+
+    // Receive data FROM the server!
+    socket.onmessage = function (evt) {
+        status.innerHTML = "Kinect data received.";
+
+        // Get the data in JSON format.
+        var jsonObject = eval('(' + evt.data + ')');
+
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.fillStyle = "#FF0000";
+        context.beginPath();
+
+        // Display the skeleton joints.
+        for (var i = 0; i < jsonObject.skeletons.length; i++) {
+            for (var j = 0; j < jsonObject.skeletons[i].joints.length; j++) {
+                var joint = jsonObject.skeletons[i].joints[j];
+
+                // Draw!!!
+                context.arc(parseFloat(joint.x), parseFloat(joint.y), 10, 0, Math.PI * 2, true);
+            }
+        }
+
+        context.closePath();
+        context.fill();
+
+        // Inform the server about the update.
+        socket.send("Skeleton updated on: " + (new Date()).toDateString() + ", " + (new Date()).toTimeString());
+    };
+};
---
+++ Kinect.Server/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Fleck;
+using Microsoft.Kinect;
+
+namespace Kinect.Server
+{
+    class Program
+    {
+        static KinectSensor _sensor;
+        static List<IWebSocketConnection> _sockets;
+
+        static bool _initialized = false;
+        static Skeleton[] _skeletons = new Skeleton[6];
+
+        static void Main(string[] args)
+        {
+            if (KinectSensor.KinectSensors.Count <= 0) return;
+
+            InitilizeKinect();
+            InitializeSockets();
+        }
+
+        private static void InitializeSockets()
+        {
+            _sockets = new List<IWebSocketConnection>();
+
+            var server = new WebSocketServer("ws://localhost:8181");
+
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    Console.WriteLine("Connected to " + socket.ConnectionInfo.ClientIpAddress);
+                    _sockets.Add(socket);
+                };
+                socket.OnClose = () =>
+                {
+                    Console.WriteLine("Disconnected from " + socket.ConnectionInfo.ClientIpAddress);
+                    _sockets.Remove(socket);
+                };
+                socket.OnMessage = message =>
+                {
+                    Console.WriteLine(message);
+                };
+            });
+
+            _initialized = true;
+
+            Console.ReadLine();
+        }
+
+        private static void InitilizeKinect()
+        {
+            _sensor = KinectSensor.KinectSensors.SingleOrDefault();
+            _sensor.SkeletonStream.Enable();
+            _sensor.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(Sensor_SkeletonFrameReady);
+            _sensor.Start();
+        }
+
+        static void Sensor_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            if (!_initialized) return;
+
+            List<Skeleton> users = new List<Skeleton>();
+
+            using (var frame = e.OpenSkeletonFrame())
+            {
+                frame.CopySkeletonDataTo(_skeletons);
+
+                foreach (var user in _skeletons)
+                {
+                    if (user.TrackingState == SkeletonTrackingState.Tracked)
+                    {
+                        users.Add(user);
+                    }
+                }
+
+                if (users.Count > 0)
+                {
+                    string json = users.Serialize();
+
+                    foreach (var socket in _sockets)
+                    {
+                        socket.Send(json);
+                    }
+                }
+            }
+        }
+    }
+}
